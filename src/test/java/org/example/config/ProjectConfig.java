2593b55c3c2846d2422f6cf59665cf59a1b46919
Vyacheslav Leonov <vyach.son@gmail.com>
2022-06-13 18:47:37 +0300

    Changed order of configs assembling

--- src/test/java/org/example/config/ProjectConfig.java
+++ src/test/java/org/example/config/ProjectConfig.java
@@ -5,8 +5,8 @@ import org.aeonbits.owner.Config;
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
         "system:properties",
-        "classpath:config/local.properties",
-        "classpath:config/remote.properties"
+        "classpath:config/remote.properties",
+        "classpath:config/local.properties"
 })
 public interface ProjectConfig extends Config {
 
